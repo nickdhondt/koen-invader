6fc7ef1daad78409e2513e5740fa687d5ee6320b
Nick D'hondt <nick.dhondt@student.howest.be>
2016-04-18 10:56:55 +0200

    Ship markup

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var io = require("socket.io")(server);
 
 app.use("/css", express.static(__dirname + "/css"));
 app.use("/js", express.static(__dirname + "/js"));
+app.use("/img", express.static(__dirname + "/img"));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
