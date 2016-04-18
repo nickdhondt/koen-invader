ff838a231128388c444d01c355f58b31a38aee3b
Hanh Pham Thi My <thi.pham@student.howest.be>
2016-04-18 10:56:43 +0200

    beweging ship

---
+++ js/app.js
@@ -0,0 +1,47 @@
+var socket = io();
+var positionChange = 0.5;
+var isLeft =false ;
+var isRight=false ;
+var isStop =false ;
+$(document).ready(function () {
+    getValues();
+});
+function getValues(){
+    socket.on("clickButton",function(){
+        //schiet
+    });
+    socket.on("startRightTilt",function(){
+       // Ga naar rechts
+        isRight = true;
+        isLeft = false;
+        isStop = false;
+    });
+    socket.on("startLeftTilt",function(){
+       //Ga naar links
+        isLeft = true;
+        isRight = false;
+        isStop = false;
+    });
+    socket.on("stopTilt", function () {
+        // Sta stil
+        isStop = true;
+        isRight = false;
+        isLeft = false;
+    });
+}
+
+function changePosition(){
+    var shipPos= $(".ship").position().left;
+    console.log(shipPos);
+    if(shipPos != 0 && shipPos !=100){
+        if(isLeft){
+            $(".ship").style.left = shipPos + positionChange +"%";
+        }
+        if(isRight){
+            $(".ship").style.left = shipPos - positionChange +"%";
+        }
+    }
+
+    window.requestAnimationFrame(changePosition);
+}
+window.requestAnimationFrame(changePosition);
